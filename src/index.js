418087fd27af0fb9cb6a79e2c9a17cd0c7f4f810
fakhreddin rezaei <fakhrad@openratio.com>
2019-02-10 12:03:53 +0330

    database init connection added to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const authserver = require('./authserver');
-const model = require('./models/authmodel')
-const oauth = new authserver({
-  model: model,
-  allowBearerTokensInQueryString: true,
-  accessTokenLifetime: process.env.ACCESSTOKEN_LIFETIME || 4 * 60 * 60
-});
-console.log('OAuth service started successfully.')
+const model = require('./models/authmodel');
+var mongoose = require('mongoose');
+var init = function()
+{
+    var dev_db_url = 'mongodb://fakhrad:logrezaee24359@ds026018.mlab.com:26018/mivapp-shopper'
+    var mongoDB = process.env.MIVAPP_FILES_DB_URL || dev_db_url;
+    mongoose.connect(mongoDB);  
+    mongoose.Promise = global.Promise;
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.on('connected', ()=>{
+      console.log('MongoDb connected');
+      const oauth = new authserver({
+          model: model,
+          allowBearerTokensInQueryString: true,
+          accessTokenLifetime: process.env.ACCESSTOKEN_LIFETIME || 4 * 60 * 60
+        });
+        console.log('Oauth service started.');
+    });
+}
+init();
+
+console.log('OAuth service starting...')
 module.exports = oauth;
