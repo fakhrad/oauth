d9a4303ec4c111c02fb0be98d9abd5e30004cd57
fakhrad <fakhrad@openratio.com>
2019-12-01 11:43:09 +0330

    issueDate added to token model

--- src/models/token.js
+++ src/models/token.js
@@ -1,24 +1,28 @@
-
 /**
  * Module dependencies.
  */
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 /**
  * Schema definitions.
  */
 
-mongoose.model('Tokens', new Schema({
-  accessToken: { type: String },
-  accessTokenExpiresOn: { type: Date },
-  client : { type: Object },  // `client` and `user` are required in multiple places, for example `getAccessToken()`
-  clientId: { type: String },
-  refreshToken: { type: String },
-  refreshTokenExpiresOn: { type: Date },
-  user : { type: Object },
-  userId: { type: String },
-  authenticated : {type : Boolean},
-  activation_code : {type : Number}
-}));
+mongoose.model(
+  "Tokens",
+  new Schema({
+    accessToken: { type: String },
+    accessTokenExpiresOn: { type: Date },
+    client: { type: Object }, // `client` and `user` are required in multiple places, for example `getAccessToken()`
+    clientId: { type: String },
+    refreshToken: { type: String },
+    refreshTokenExpiresOn: { type: Date },
+    user: { type: Object },
+    userId: { type: String },
+    authenticated: { type: Boolean },
+    activation_code: { type: Number },
+    token: { type: String },
+    issueDate: { type: Date, default: new Date() }
+  })
+);
